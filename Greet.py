deac056cf22f48c9d0f454f22db929d54f9276a8
THDVaibhav <vaibhavverma210090@gmail.com>
2026-05-24 14:49:35 +0530

    Add greeting functions to Greet.py

---
+++ Greet.py
@@ -0,0 +1,13 @@
+n = input("Enter your name: ")
+
+def greet():
+    print("Have a good day", n)
+
+greet()
+
+pass
+
+def greeting(name,ending="Thank You"):
+    print("Have a good day", name)
+    print(ending)
+greeting("Vaibhav","Thanks")
